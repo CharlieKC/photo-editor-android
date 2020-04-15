ea077a71d37218faced5b52dbb1841f63c77f980
CharlieKC <charlie.kruczko@gmail.com>
2020-04-15 13:46:52 +0100

    Update BrushDrawingView.java

--- photoeditorsdk/src/main/java/com/ahmedadeltito/photoeditorsdk/BrushDrawingView.java
+++ photoeditorsdk/src/main/java/com/ahmedadeltito/photoeditorsdk/BrushDrawingView.java
@@ -20,7 +20,7 @@ import android.view.View;
 
 public class BrushDrawingView extends View {
 
-    private float brushSize = 200;
+    private float brushSize = 3;
     private float brushEraserSize = 100;
 
     private Path drawPath;
